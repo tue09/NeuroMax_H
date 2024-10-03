5affde235fee8e0774643369eca10a2ee7474c49
letue09 <tue209003@gmail.com>
2024-10-03 16:25:25 +0700

    kk

--- FASTopic/_model_utils.py
+++ FASTopic/_model_utils.py
@@ -1,9 +1,6 @@
 import torch
 
 def pairwise_euclidean_distance(x, y):
-    if x.size(1) != y.size(1):
-        cost = torch.sum(x ** 2, axis=1, keepdim=True) + torch.sum(y ** 2, dim=1) - 2 * torch.matmul(x.t(), y.t())
-    else:
-        cost = torch.sum(x ** 2, axis=1, keepdim=True) + torch.sum(y ** 2, dim=1) - 2 * torch.matmul(x, y.t())
+    cost = torch.sum(x ** 2, axis=1, keepdim=True) + torch.sum(y ** 2, dim=1) - 2 * torch.matmul(x, y.t()).t()
     return cost
 

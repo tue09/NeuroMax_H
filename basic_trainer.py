5c2a930692b533c2966f06583d513a34b2ea4601
letue09 <tue209003@gmail.com>
2024-10-04 23:18:30 +0700

    r

--- basic_trainer.py
+++ basic_trainer.py
@@ -124,6 +124,7 @@ class BasicTrainer:
                 if (batch_id + 1) % accumulation_steps == 0 or (batch_id + 1) == len(dataset_handler.train_dataloader):
                     #theta, _ = self.model.encode(batch_data[0].to('cuda'))
                     #loss_ctr_ = self.model.get_loss_CTR(theta, indices)
+                    print(f"SAM name = {self.SAM_name}")
                     if self.SAM_name == 'TRAM':
                         loss_ctr_ = self.model.get_loss_CTR(batch_data, indices)
                         sam_optimizer.first_step(loss_ctr_,

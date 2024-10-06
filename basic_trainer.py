56f4c29e18d14ff710684490542167b960bb85e9
letue09 <tue209003@gmail.com>
2024-10-06 19:25:27 +0700

    rrt

--- basic_trainer.py
+++ basic_trainer.py
@@ -124,8 +124,8 @@ class BasicTrainer:
                 # theta = self.model.get_theta(batch_data_tensor)
 
                 if self.use_SAM == 0:
-                    adam_optimizer.zero_grad()
                     adam_optimizer.step()
+                    adam_optimizer.zero_grad()
                 else:
                     if (batch_id + 1) % accumulation_steps == 0 or (batch_id + 1) == len(dataset_handler.train_dataloader):
                         #theta, _ = self.model.encode(batch_data[0].to('cuda'))

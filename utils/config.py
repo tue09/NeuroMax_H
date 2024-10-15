890cb0573ea5effe03a8a21e2b291c1e7dbe96bc
letue09 <tue209003@gmail.com>
2024-10-15 20:42:22 +0700

    rrr

--- utils/config.py
+++ utils/config.py
@@ -31,7 +31,7 @@ def add_model_argument(parser):
     parser.add_argument('--beta_temp', type=float, default=0.2)
     parser.add_argument('--threshold', type=float, default=10)
     parser.add_argument('--render', type=int, default=0)
-    parser.add_argument('--step', type=float, default=150)
+    parser.add_argument('--step', type=int, default=150)
     parser.add_argument('--alpha_range', type=float, default=1.5)
 
 

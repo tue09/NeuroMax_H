33af07ea9bf1ec965edbdd03a65b3f8c551795b1
letue09 <tue209003@gmail.com>
2024-11-02 18:36:43 +0700

    rr

--- FASTopic/CTR.py
+++ FASTopic/CTR.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 
 
-class OT(nn.Module):
+class CTR(nn.Module):
     def __init__(self, weight_loss_OT, sinkhorn_alpha, OT_max_iter=5000, stopThr=.5e-2):
         super().__init__()
 

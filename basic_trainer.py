d032051ea3f91f286e77c1f64537b6760b70de55
letue09 <tue209003@gmail.com>
2024-11-15 23:11:49 +1030

    ttt

--- basic_trainer.py
+++ basic_trainer.py
@@ -162,9 +162,10 @@ class BasicTrainer:
                 if self.use_SAM == 0:
                     if epoch > self.epoch_threshold:
                         if self.use_MOO == 1:
+                            loss_array2 = [value for key, value in rst_dict.items() if 'loss_' not in key]
                             loss_array = [value for key, value in rst_dict.items() if 'loss_x' in key]
                             if (epoch % 10 == 0) and (batch_id == 0):
-                                loss_values = [value.item() for value in loss_array]
+                                loss_values = [value.item() for value in loss_array2]
                                 print(f"Loss array = {loss_values}")
                             grad_array = [grad_decomposer._get_total_grad(loss_) for loss_ in loss_array]
                             adjusted_grad, alpha = moo_algorithm.apply(grad_array)
